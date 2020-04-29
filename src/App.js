9d45b611db4c2a2573c98bdf05de60f7688338c2
github username <github email address>
2020-04-29 10:54:42 -0600

    fourth try

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 import './App.css'
 import MainContainer from './components/MainContainer';
+import Favorites from './components/Favorites'
 
 export default class App extends Component {
   constructor(){
     super()
     this.state = {
-      characters: []
+      characters: [],
+      favorites: []
     }
   }
 
@@ -18,11 +20,17 @@ export default class App extends Component {
       }))
   }
 
+  addToFavorites = (character) => {
+    if(this.state.favorites.find(char => char === character))
+    this.setState({favorites: [...this.state.favorites, character]})
+  }
+
   render(){
     return(
       <div className='App'>
+        <Favorites favorites={this.state.favorites}/>
         <h1>Rick and Morty Characters</h1>
-        <MainContainer characters={this.state.characters}/>
+        <MainContainer characters={this.state.characters} addToFavorites={this.addToFavorites}/>
       </div>
     )
   }
